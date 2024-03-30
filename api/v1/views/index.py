764ab359461f049808422d4cb6b3b824f0ec46c9
La Victoire <lavictoire633@gmail.com>
2024-03-30 13:24:27 -0700

    making corrections to the wildcard importing of the api.views.index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This is the index"""
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
     """Returns the status of the request"""
-    return '{"status": "OK"}'
+    return jsonify({"status": "OK"})
