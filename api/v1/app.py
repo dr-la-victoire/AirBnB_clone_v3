48f42e7409ebc589744196eb62a0bdd971c76345
La Victoire <lavictoire633@gmail.com>
2024-03-30 13:06:24 -0700

    adding just the default host and port for debuging purposes

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,4 @@ def destroy_db():
 
 
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = os.environ.get('HBNB_API_PORT', '5000')
-    app.run(host, port, threaded=True)
+    app.run(host=0.0.0.0 port=5000, threaded=True)
